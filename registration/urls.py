3003f70db2449b8261833b8ba2c86ced60abc415
Deadlymind <oussamaayari2014@gmail.com>
2024-01-27 17:30:42 +0100

    format readme

--- registration/urls.py
+++ registration/urls.py
@@ -3,21 +3,6 @@ from django.urls import path
 from .views import submit_form, success_page
 
 urlpatterns = [
-    """
-    URL pattern for submitting the registration form.
-
-    Example:
-    - http://yourdomain.com/register/
-    """
     path('register/', submit_form, name='register_form'),
-
-    """
-    URL pattern for displaying the success page after form submission.
-
-    Example:
-    - http://yourdomain.com/success/
-    
-    Note: 'success_page' should match the name used in the redirect.
-    """
     path('success/', success_page, name='success_page'),
 ]
